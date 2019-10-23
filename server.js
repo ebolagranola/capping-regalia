cdb6248207868794ee721eb02827036cff50eaa0
DaleDoster <dale.doster1@marist.edu>
2019-10-23 19:39:52 -0400

    Traded main card for full top navbar

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static('./client/'));
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/wireframe.png', function(req, res) {
-    res.sendFile(path.join(__dirname + '/wireframe.png'));
+    res.sendFile(path.join(__dirname + './client/images/wireframe.png'));
 });
 
 app.listen(1000);

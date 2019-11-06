4b26116dc12f80102b3391c246ffb1df65608ed0
Oliver Fountain <Oliver.Fountain1@marist.edu>
2019-11-06 13:19:18 -0500

    Changed the text in the email for demo

--- Visual Studio Capping Project/Visual Studio Capping Project/Homepage.aspx.cs
+++ Visual Studio Capping Project/Visual Studio Capping Project/Homepage.aspx.cs
@@ -190,8 +190,8 @@ namespace Visual_Studio_Capping_Project
 
         public static void SendEmail(String email, String name) {
             MailMessage mailMessage = new MailMessage("maristregalia@gmail.com", email);
-            mailMessage.Subject = "Look At Me!";
-            mailMessage.Body = "Hey " + name + ", I sent this email from our website, how you like me now?";
+            mailMessage.Subject = "Your order on MaristReglia.site";
+            mailMessage.Body = "Hey " + name + ", we have recived your order and put the information in our database";
 
             SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
             smtpClient.Credentials = new System.Net.NetworkCredential()
